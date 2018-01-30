f3b24e413ebae00cdafb1780f99d7f378f0ceac3
yuningbao <yuning.bao@outlook.com>
2018-01-30 16:11:01 +0100

    correction

--- src/camera/choixcamera.py
+++ src/camera/choixcamera.py
@@ -1,13 +1,14 @@
 import numpy as np
 import cv2
 import glob
+import position_detect
 
 
 
 #piece High=4m Large=8.5m Wide=5m
 
 
-#function : change from the coordinate system camera to coordinate used by group trajectory 
+#function : change from the coordinate system camera to coordinate used by group trajectory
 def change_repere(x,y,z,position):
     xyz=position[0]
     angle=position[1]
@@ -31,22 +32,22 @@ def change_repere(x,y,z,position):
     return noimg,x_abs,y_abs,z_abs,yaw,pitch,rall
 
 
-#function : whether chessboard is too inclined / is captured by camera   
+#function : whether chessboard is too inclined / is captured by camera
 def quality(position,limite_pitch,limite_yaw):
     bon=1
     axis_rotation=position[2]
     noimg=position[3]
 
-    #if not captured by camera 
+    #if not captured by camera
     if noimg==1:
         bon=0
 
-    #if captured, 
-    #imite_yaw and limite_pitch are defined by characteristic, obtain by experiment 
+    #if captured,
+    #imite_yaw and limite_pitch are defined by characteristic, obtain by experiment
 
     elif abs(axis_rotation[1])>(np.cos(limite_yaw)*np.sin(limite_pitch) or abs(axis_rotation[2])>np.sin(limite_yaw)):
         bon=0
-    return bon 
+    return bon
 
 
 
@@ -74,19 +75,19 @@ while(1):
 
 
 
-    for j in range(4): 
-        position_j=fnpostion(dist[j],mtx[j],cap)
+    for j in range(4):
+        position_j=position_detect.fnpostion(dist[j],mtx[j],cap[j])
 
-        #defalut : 60 degree for limite_pitch and limite_yaw 
+        #defalut : 60 degree for limite_pitch and limite_yaw
         if quality(position_j,np.pi/3,np.pi/3):
             position_ab_j=change_repere(0,camera_y[j],camera_z[j],position_j)
             position=np.add(position,position_ab_j)
 
-    position=position/4
+    position=[c/4.0 for c in position]
     x_abs,y_abs,z_abs,yaw,pitch,rall=position[1:7]
-    
+
     print ('-----position------')
-    print (x_abs,y_abs,z_abs)  
+    print (x_abs,y_abs,z_abs)
 
     print ('-------rotation--------')
     print (yaw,pitch,rall)
