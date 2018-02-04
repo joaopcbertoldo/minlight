2d37005e4b42f01861c5346c997a13c405efed65
Billy Everyteen <joaopcbertoldo@gmail.com>
2018-02-04 23:05:33 +0100

    pbm in tension

--- src/models/tension.py
+++ src/models/tension.py
@@ -1,13 +1,11 @@
-from src.models.entites_systeme_minlight import Cable
-from src.math_entities import Vec3
-
-from numpy import sqrt
-from numpy.linalg import inv, det
 import numpy as np
 
+from src.math_entities import Vec3
+from src.models.entites_systeme_minlight import Cable
 
-def get_tension (cable0, cable1, cable2, cable3, cable4, cable5, cable6, cable7):
-    '''
+
+def get_tension(cable0, cable1, cable2, cable3, cable4, cable5, cable6, cable7):
+    """
     Function to calculate the tension in each cable.
     Reference: "Closed-form force distribution for parallel wire robots",
     A. Pott, T. Bruckmann, and L. Mikelsons
@@ -15,13 +13,13 @@ def get_tension (cable0, cable1, cable2, cable3, cable4, cable5, cable6, cable7)
     :param cable: Instances of the Cable class.
 
     :return: F, a np.array containing 8 tension values (in Newtons).
-    '''
+    """
 
     # Source's mass and Earth's gravity
     m = 50.0  # kg
     g = 9.8  # m/s^2
 
-    F_min = np.array([cable0.get_tension_min(),
+    f_min = np.array([cable0.get_tension_min(),
                       cable1.get_tension_min(),
                       cable2.get_tension_min(),
                       cable3.get_tension_min(),
@@ -30,7 +28,7 @@ def get_tension (cable0, cable1, cable2, cable3, cable4, cable5, cable6, cable7)
                       cable6.get_tension_min(),
                       cable7.get_tension_min()])
 
-    F_max = np.array([cable0.get_tension_max(),
+    f_max = np.array([cable0.get_tension_max(),
                       cable1.get_tension_max(),
                       cable2.get_tension_max(),
                       cable3.get_tension_max(),
@@ -39,7 +37,7 @@ def get_tension (cable0, cable1, cable2, cable3, cable4, cable5, cable6, cable7)
                       cable6.get_tension_max(),
                       cable7.get_tension_max()])
 
-    # Equilibrium equation: A^t . F + w = 0, F_min < Fi < F_max, A^t = transpose(A)
+    # Equilibrium equation: A^t . F + w = 0, f_min < Fi < f_max, A^t = transpose(A)
 
     # normalized cable vectors
     u0 = cable0.get_direction_source_to_fixed()
@@ -61,48 +59,44 @@ def get_tension (cable0, cable1, cable2, cable3, cable4, cable5, cable6, cable7)
     b6 = cable6.get_sommet_source() - centre_masse
     b7 = cable7.get_sommet_source() - centre_masse
 
-
     A = np.array([
-        [np.append(u0, b0.cross(u0))],
-        [np.append(u1, b1.cross(u1))],
-        [np.append(u2, b2.cross(u2))],
-        [np.append(u3, b3.cross(u3))],
-        [np.append(u4, b4.cross(u4))],
-        [np.append(u5, b5.cross(u5))],
-        [np.append(u6, b6.cross(u6))],
-        [np.append(u7, b7.cross(u7))]
-        ]).reshape(8, 6)
+        [np.append(u0, np.cross(b0, u0))],
+        [np.append(u1, np.cross(b1, u1))],
+        [np.append(u2, np.cross(b2, u2))],
+        [np.append(u3, np.cross(b3, u3))],
+        [np.append(u4, np.cross(b4, u4))],
+        [np.append(u5, np.cross(b5, u5))],
+        [np.append(u6, np.cross(b6, u6))],
+        [np.append(u7, np.cross(b7, u7))]
+    ]).reshape(8, 6)
 
     w = np.array([0, 0, -m * g, 0, 0, 0])
 
     # algorithme retourne np.array[-1.,-1.,-1.,-1.,-1.,-1.,-1.,-1.] si la position n'appartient pas
     # au workspace de la chambre
 
-
-    if (np.linalg.matrix_rank(A) < 6):
-        return -1*np.ones(8)
+    if np.linalg.matrix_rank(A) < 6:
         print("Wrench matrix not invertible")
+        return -1*np.ones(8)
 
-    F_med = (F_min+F_max)/2
+    f_med = (f_min+f_max)/2
 
     A_pseudo_transp = np.dot(A, np.linalg.inv(np.dot(np.transpose(A), A)))
 
-    F_v = - np.dot(A_pseudo_transp, w + np.dot(np.transpose(A), F_med))
+    F_v = - np.dot(A_pseudo_transp, w + np.dot(np.transpose(A), f_med))
 
-    F = F_med + F_v
+    F = f_med + F_v
 
 #    for i in range(8):
-#        if (F[i] < F_min[i] or F[i] > F_max[i]):
+#        if (F[i] < f_min[i] or F[i] > f_max[i]):
 #            print("Cable {} is not in the interval [f_min, f_max]".format(i))
 #            return -1 * np.ones(8)
 
-#    if (np.linalg.norm(F_v) > np.linalg.norm(F_med) / 2):
+#    if (np.linalg.norm(F_v) > np.linalg.norm(f_med) / 2):
 #        print("Tension not feasible")
 #        return -1 * np.ones(8)
 
     return F
-
-
 
 
 # TESTE:
@@ -114,7 +108,8 @@ point_ancrage = [Vec3(0.0, 0.0, 5.0), Vec3(4.0, 0.0, 5.0),
                  Vec3(4.0, 6.0, 5.0), Vec3(0.0, 6.0, 5.0)]
 
 centre_masse = Vec3(2.0, 3.0, 2.5)
-#sommet_source = [centre_masse + np.array([-long / 2, -larg / 2, -haut / 2]),
+
+# sommet_source = [centre_masse + np.array([-long / 2, -larg / 2, -haut / 2]),
 #                 centre_masse + np.array([ long / 2, -larg / 2, -haut / 2]),
 #                 centre_masse + np.array([ long / 2,  larg / 2, -haut / 2]),
 #                 centre_masse + np.array([-long / 2,  larg / 2, -haut / 2]),
@@ -122,15 +117,17 @@ centre_masse = Vec3(2.0, 3.0, 2.5)
 #                 centre_masse + np.array([ long / 2, -larg / 2,  haut / 2]),
 #                 centre_masse + np.array([ long / 2,  larg / 2,  haut / 2]),
 #                 centre_masse + np.array([-long / 2,  larg / 2,  haut / 2])]
-sommet_source = [   Vec3(1.5, 2.5, 2.0),
-                    Vec3(2.5, 2.5, 2.0),
-                    Vec3(2.5, 3.5, 2.0),
-                    Vec3(1.5, 3.5, 2.0),
-                    Vec3(1.5, 2.5, 3.0),
-                    Vec3(2.5, 2.5, 3.0),
-                    Vec3(2.5, 3.5, 3.0),
-                    Vec3(1.5, 3.5, 3.0),
-                 ]
+
+sommet_source = [
+    Vec3(1.5, 2.5, 2.0),
+    Vec3(2.5, 2.5, 2.0),
+    Vec3(2.5, 3.5, 2.0),
+    Vec3(1.5, 3.5, 2.0),
+    Vec3(1.5, 2.5, 3.0),
+    Vec3(2.5, 2.5, 3.0),
+    Vec3(2.5, 3.5, 3.0),
+    Vec3(1.5, 3.5, 3.0),
+]
 
 cable0 = Cable(sommet_source[4], "S001", point_ancrage[0], 1., 10., 100.)
 cable1 = Cable(sommet_source[5], "S101", point_ancrage[0], 1., 10., 100.)
